2f39fa4d8db692759c3a98a861df0b4c06f83558
AndreaCollura <andrea.collur@gmail.com>
2023-04-13 15:01:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <header></header>
+  <main></main>
+  <footer></footer>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    components: {
+
+    }
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
