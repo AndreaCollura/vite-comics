5d45ea2aa4c36f6d82e8e8c1acee870466d57457
AndreaCollura <andrea.collur@gmail.com>
2023-04-14 17:16:55 +0200

    add card component to main component

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,28 +1,42 @@
 <template>
-    <div class="col-12">
-        <div class="card">
-            <div class="card-body">
-                <div class="card-image">
-                    <img :src="" alt="">
-                </div>
-                <div class="card-title">
-                    {{ title }}
-                </div>
-            </div>
+    <div class="comic-box d-flex flex-column ms-2 mt-2">
+        <div class="comic">
+            <img :src="image">
         </div>
-
-
+        <div class="mt-3" >
+            <span class="text-white text-uppercase small">{{title}}</span>
+        </div>
     </div>
+    
 </template>
 
 <script>
-    export default {
-        name: 'CardComponent',
-        props: ['image', 'title']
-        
-    }
+export default {
+    name: 'CardComponent',
+    props: ['image', 'title']
+
+}
 </script>
 
 <style lang="scss" scoped>
+    .comic-box{
+        width: 180px;
+    }
+    
 
+    .comic{
+        width: 180px;
+        height: 170px;
+        overflow-y: hidden;
+
+    }
+    img{
+        width: 100%;
+        
+    }
+    span{
+        font-size: .7rem;
+    }
+    
+    
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,11 +1,10 @@
 <template>
-    <JumboComponent/>
+    <JumboComponent />
     <div class="content-bg">
         <div class="container py-4 px-0 ">
-            <div class="d-flex justify-content-start m-0">
-                <div>
-                    <h2 class="py-3 text-white"><i class="fa-solid fa-arrow-right "></i>Content goes here<i class="fa-solid fa-arrow-left"></i></h2>
-                </div>
+            <div class="d-flex flex-wrap justify-content-between gap-3 ms-4">
+                <CardComponent :image="comic.thumb" :title="comic.series" v-for="(comic,index) in comicSeries" :key="index"/>
+                
             </div>
         </div>
     </div>
@@ -15,8 +14,8 @@
                 <!-- fare un v-for -->
                 <ul class="d-flex justify-content-around ps-0 m-0">
                     <li v-for="(link, index) in shopList" :key="index" class="d-flex align-items-center ">
-                        <img  :src="link.icon" alt="">
-                        <span class="text-uppercase text-white ms-1 ">{{link.text}}</span>
+                        <img :src="link.icon" alt="">
+                        <span class="text-uppercase text-white ms-1 ">{{ link.text }}</span>
                     </li>
                 </ul>
             </div>
@@ -25,48 +24,53 @@
 </template>
 
 <script>
+import CardComponent from './CardComponent.vue'
 import JumboComponent from './JumboComponent.vue';
 import { mainLinks } from '../data/data.js';
-    export default {
-        name: 'MainComponent',
-        data(){
-            return {
-                shopList: mainLinks,
-
-            }
-        },
-        components: {
-            JumboComponent,
+import { dcComics } from '../data/data.js';
+export default {
+    name: 'MainComponent',
+    data() {
+        return {
+            comicSeries: [...dcComics],
+            shopList: mainLinks,
 
         }
+    },
+    components: {
+        JumboComponent,
+        CardComponent
 
-        
     }
+
+
+}
 </script>
 
 <style lang="scss" scoped>
 @use '../assets/styles/partials/variables' as *;
-    .content-bg{
-        background-color: #1c1c1c;
-    }
 
-    .iconBox{
-        background-color: $blue-bg;
-        // height: 140px;
+.content-bg {
+    background-color: #1c1c1c;
+}
 
-    }
+.iconBox {
+    background-color: $blue-bg;
+    // height: 140px;
 
-    img{
-        width: 20%;
-    }
+}
 
-    span{
-        font-size: .8rem;
-        white-space: nowrap;
-    }
+img {
+    width: 20%;
+}
+
+span {
+    font-size: .8rem;
+    white-space: nowrap;
+}
 
 
-    ul{
-        list-style: none;
-    }
+ul {
+    list-style: none;
+}
 </style>
